a84408c4357be5e699880039da489bd3608afb55
Drew Reed <111479240+drewreed2005@users.noreply.github.com>
2026-02-27 11:55:09 -0800

    super mario bros. japan (script/html compatibilty test)

---
+++ script.js
@@ -0,0 +1,4 @@
+function dropBomb() {
+    document.getElementById("bomb_zone_3000")
+        .innerHTML = "oh the pain it burns it burns<br>-japanese civilian<br><br>why have you done this<br>-luigi from mario";
+}
